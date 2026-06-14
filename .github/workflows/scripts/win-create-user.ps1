cf172dafe02cb4eb2b95d0888a9965b349391416
asdddsa101 <asdddsa101@outlook.com>
2026-06-14 16:40:30 +0800

    Update win-create-user.ps1

--- .github/workflows/scripts/win-create-user.ps1
+++ .github/workflows/scripts/win-create-user.ps1
@@ -7,8 +7,9 @@ $charSet = @{
   Special = ([char[]](33..47) + [char[]](58..64) + [char[]](91..96) + [char[]](123..126))
 }
 $rawPassword = @()
-$rawPassword += $charSet.Upper | Get-Random -Count 4
-$rawPassword += $charSet.Number | Get-Random -Count 4
+$rawPassword += $charSet.Lower | Get-Random -Count 3
+$rawPassword += $charSet.Upper | Get-Random -Count 3
+$rawPassword += $charSet.Number | Get-Random -Count 2
 $password = -join ($rawPassword | Sort-Object { Get-Random })
 $securePass = ConvertTo-SecureString $password -AsPlainText -Force
 New-LocalUser -Name "zzz" -Password $securePass -AccountNeverExpires
